5a6a7ed93c61070ce7300ccfa7b2f1437a7e2424
thoriqmacto <thariq.hadad@gmail.com>
2024-10-03 04:17:43 +0700

    adding search and pagination

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,15 +1,14 @@
-import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices, fetchCardData} from "@/app/lib/data";
-import {Suspense} from "react";
-import {RevenueChartSkeleton} from "@/app/ui/skeletons";
+import { Suspense } from "react";
+import { RevenueChartSkeleton,LatestInvoicesSkeleton, CardsSkeleton  } from "@/app/ui/skeletons";
+import CardWrapper from "@/app/ui/dashboard/cards";
 
 export default async function Page() {
     // const revenue = await fetchRevenue();
-    const latestInvoices = await fetchLatestInvoices();
-    const {numberOfCustomers,numberOfInvoices,totalPaidInvoices,totalPendingInvoices} = await fetchCardData();
+    // const latestInvoices = await fetchLatestInvoices();
+    // const {numberOfCustomers,numberOfInvoices,totalPaidInvoices,totalPendingInvoices} = await fetchCardData();
     return (
         <main>
             <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -17,14 +16,17 @@ export default async function Page() {
             </h1>
 
             <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-                <Card title="Collected" value={totalPaidInvoices} type="collected" />
-                <Card title="Pending" value={totalPendingInvoices} type="pending" />
-                <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-                <Card
-                      title="Total Customers"
-                      value={numberOfCustomers}
-                      type="customers"
-                />
+                {/*<Card title="Collected" value={totalPaidInvoices} type="collected" />*/}
+                {/*<Card title="Pending" value={totalPendingInvoices} type="pending" />*/}
+                {/*<Card title="Total Invoices" value={numberOfInvoices} type="invoices" />*/}
+                {/*<Card*/}
+                {/*      title="Total Customers"*/}
+                {/*      value={numberOfCustomers}*/}
+                {/*      type="customers"*/}
+                {/*/>*/}
+                <Suspense fallback={<CardsSkeleton/>}>
+                    <CardWrapper/>
+                </Suspense>
             </div>
 
             <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
@@ -32,7 +34,9 @@ export default async function Page() {
                 <Suspense fallback={<RevenueChartSkeleton/>}>
                     <RevenueChart />
                 </Suspense>
-                <LatestInvoices latestInvoices={latestInvoices} />
+                <Suspense fallback={<LatestInvoicesSkeleton/>}>
+                    <LatestInvoices/>
+                </Suspense>
             </div>
         </main>
     );
